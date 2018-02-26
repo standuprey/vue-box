459e613b3b04215e4e79a3aaf2214ee76e7d0732
DiscRiskandBisque <joshquin@gmail.com>
2018-02-26 17:12:15 -0500

    React -> vue in readme, removed from address in config

--- truffle.js
+++ truffle.js
@@ -5,7 +5,6 @@ module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
-      from: '0xb99bb97607ee5a232ff32e542192080394599984',
       port: 8545,
       network_id: '*', // Match any network id
       gas: 470000
